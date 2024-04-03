a7460e468f7843d6dc2f3e6e86ef30a90e7ec0a4
ZhonghaoWei <104274390+ZhonghaoWei@users.noreply.github.com>
2024-04-02 23:32:49 -0400

    Code Update

--- amplifyapp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplifyapp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "fullstackreactapplic": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "fullstackreactapplic77eb9441": {
       "AppClientID": "string",
