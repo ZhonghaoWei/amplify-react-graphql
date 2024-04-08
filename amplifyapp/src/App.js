40f38107aeb355b933063ad6850916773d21becd
your_username <pad128g@icloud.com>
2024-04-08 15:55:48 -0400

    Update App.js

--- amplifyapp/src/App.js
+++ amplifyapp/src/App.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
-import { generateClient} from 'aws-amplify/api';
+import { generateClient } from "aws-amplify/api";
 //import { Amplify } from 'aws-amplify';
-import { getUrl } from 'aws-amplify/storage';
-import { remove } from 'aws-amplify/storage';
-import { uploadData } from 'aws-amplify/storage';
+import { getUrl } from "aws-amplify/storage";
+import { remove } from "aws-amplify/storage";
+import { uploadData } from "aws-amplify/storage";
 import {
   Button,
   Flex,
@@ -52,9 +52,11 @@ const App = ({ signOut }) => {
     const data = {
       name: form.get("name"),
       description: form.get("description"),
-      image: image.name,
+      image: image?.name,
     };
-    if (!!data.image) await uploadData(data.name, image);
+
+    console.log("data", data);
+    if (!!data.image) uploadData(data.name, image);
     await client.graphql({
       query: createTodoMutation,
       variables: { input: data },
@@ -62,25 +64,21 @@ const App = ({ signOut }) => {
     fetchTodos();
     event.target.reset();
   }
-  
+
   async function deleteTodo({ id, name }) {
     const newTodos = notes.filter((note) => note.id !== id);
     setTodos(newTodos);
-    await remove({name});
+    await remove({ name });
     await client.graphql({
       query: deleteTodoMutation,
       variables: { input: { id } },
     });
   }
 
-
-
   return (
-    <><View
-      name="image"
-      as="input"
-      type="file"
-      style={{ alignSelf: "end" }} /><View className="App">
+    <>
+      <View name="image" as="input" type="file" style={{ alignSelf: "end" }} />
+      <View className="App">
         <Heading level={1}>My Todos App</Heading>
         <View as="form" margin="3rem 0" onSubmit={createTodo}>
           <Flex direction="row" justifyContent="center">
@@ -90,14 +88,16 @@ const App = ({ signOut }) => {
               label="Note Name"
               labelHidden
               variation="quiet"
-              required />
+              required
+            />
             <TextField
               name="description"
               placeholder="Note Description"
               label="Note Description"
               labelHidden
               variation="quiet"
-              required />
+              required
+            />
             <Button type="submit" variation="primary">
               Create Note
             </Button>
@@ -105,32 +105,33 @@ const App = ({ signOut }) => {
         </View>
         <Heading level={2}>Current Todos</Heading>
         <View margin="3rem 0">
-        {notes.map((note) => (
-  <Flex
-    key={note.id || note.name}
-    direction="row"
-    justifyContent="center"
-    alignItems="center"
-  >
-    <Text as="strong" fontWeight={700}>
-      {note.name}
-    </Text>
-    <Text as="span">{note.description}</Text>
-    {/* {note.image && (
+          {notes.map((note) => (
+            <Flex
+              key={note.id || note.name}
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Text as="strong" fontWeight={700}>
+                {note.name}
+              </Text>
+              <Text as="span">{note.description}</Text>
+              {/* {note.image && (
       <Image
         src={note.image}
         alt={`visual aid for ${notes.name}`}
         style={{ width: 400 }}
       />
     )} */}
-    {/* <Button variation="link" onClick={() => deleteNote(note)}>
+              {/* <Button variation="link" onClick={() => deleteNote(note)}>
       Delete note
     </Button> */}
-  </Flex>
-))}
+            </Flex>
+          ))}
         </View>
         <Button onClick={signOut}>Sign Out</Button>
-      </View></>
+      </View>
+    </>
   );
 };
 
